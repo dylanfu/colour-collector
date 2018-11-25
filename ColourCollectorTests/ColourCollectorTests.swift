6cc69b9dbae675a1ddedf4fb9f043e61f4fa51e0
Dylan Fu <dylan.fu@hotmail.co.nz>
2018-11-25 18:14:16 +1300

    Initial Commit

---
+++ ColourCollectorTests/ColourCollectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  ColourCollectorTests.swift
+//  ColourCollectorTests
+//
+//  Created by Dylan Fu on 25/11/18.
+//  Copyright © 2018 Dylan Fu. All rights reserved.
+//
+
+import XCTest
+@testable import ColourCollector
+
+class ColourCollectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
